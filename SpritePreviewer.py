1dc3f93148837a19fe89d4c3ac35c191d9f5a344
Johnny He <u1394005@utah.edu>
2025-04-15 14:27:50 -0600

    test

--- SpritePreviewer.py
+++ SpritePreviewer.py
@@ -18,7 +18,7 @@ class SpritePreview(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Sprite Animation Preview")
+        self.setWindowTitle("Sprite Preview")
         # This loads the provided sprite and would need to be changed for your own.
         self.num_frames = 21
         self.frames = load_sprite('spriteImages', self.num_frames)
@@ -26,7 +26,12 @@ class SpritePreview(QMainWindow):
         # Add any other instance variables needed to track information as the program
         # runs here
         self.current_fps = 30
+        self.current_frame_index = 0
         self.is_playing = False
+
+        # Timer to control sprite animation playback
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_frame)
 
         # Make the GUI in the setupUI method
         self.setupUI()
@@ -43,41 +48,47 @@ class SpritePreview(QMainWindow):
 
         main_layout = QVBoxLayout()
 
-        # Sprite image display (empty label with border)
+        # Top horizontal layout: sprite image and vertical slider
+        top_layout = QHBoxLayout()
+
+        # Sprite image display
         self.image_label = QLabel()
         self.image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.image_label.setFixedSize(200, 200)
-        main_layout.addWidget(self.image_label)
+        self.image_label.setFixedSize(150, 150)
+        top_layout.addWidget(self.image_label)
 
-        # FPS slider and labels
-        fps_layout = QHBoxLayout()
-
-        self.fps_text_label = QLabel("Frames per second:")
-        fps_layout.addWidget(self.fps_text_label)
-
-        self.fps_slider = QSlider(Qt.Orientation.Horizontal)
+        # FPS vertical slider
+        self.fps_slider = QSlider(Qt.Orientation.Vertical)
         self.fps_slider.setMinimum(1)
         self.fps_slider.setMaximum(100)
         self.fps_slider.setValue(self.current_fps)
+        self.fps_slider.setTickPosition(QSlider.TickPosition.TicksBothSides)
+        self.fps_slider.setTickInterval(10)
         self.fps_slider.valueChanged.connect(self.update_fps_label)
-        fps_layout.addWidget(self.fps_slider)
+        top_layout.addWidget(self.fps_slider)
 
-        self.current_fps_label = QLabel(str(self.current_fps) + " FPS")
+        main_layout.addLayout(top_layout)
 
-        fps_layout.addWidget(self.current_fps_label)
+        # FPS label and current value
+        fps_layout = QHBoxLayout()
+        self.fps_text_label = QLabel("Frames per second")
+        fps_layout.addWidget(self.fps_text_label, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        self.current_fps_label = QLabel(str(self.current_fps))
+        fps_layout.addWidget(self.current_fps_label, alignment=Qt.AlignmentFlag.AlignCenter)
 
         main_layout.addLayout(fps_layout)
 
         # Start/Stop button
         self.start_stop_button = QPushButton("Start")
         self.start_stop_button.clicked.connect(self.toggle_animation)
-        main_layout.addWidget(self.start_stop_button)
+        main_layout.addWidget(self.start_stop_button, alignment=Qt.AlignmentFlag.AlignCenter)
 
         # Set layout and central widget
         frame.setLayout(main_layout)
         self.setCentralWidget(frame)
 
-        # Menu bar
+        # Menu bar with Pause and Exit
         menu = self.menuBar()
         file_menu = menu.addMenu("File")
 
@@ -94,16 +105,30 @@ class SpritePreview(QMainWindow):
 
     def update_fps_label(self):
         self.current_fps = self.fps_slider.value()
-        self.current_fps_label = QLabel(str(self.current_fps) + " FPS")
-
+        self.current_fps_label.setText(str(self.current_fps))
+        if self.timer.isActive():
+            delay = int(1000 / self.current_fps)
+            self.timer.start(delay)
 
     def toggle_animation(self):
-        self.is_playing = not self.is_playing
-        self.start_stop_button.setText("Stop" if self.is_playing else "Start")
+        if self.is_playing:
+            self.timer.stop()
+            self.start_stop_button.setText("Start")
+            self.is_playing = False
+        else:
+            delay = int(1000 / self.current_fps)
+            self.timer.start(delay)
+            self.start_stop_button.setText("Stop")
+            self.is_playing = True
 
     def pause_animation(self):
-        self.is_playing = False
+        self.timer.stop()
         self.start_stop_button.setText("Start")
+        self.is_playing = False
+
+    def update_frame(self):
+        self.image_label.setPixmap(self.frames[self.current_frame_index])
+        self.current_frame_index = (self.current_frame_index + 1) % self.num_frames
 
 
 def main():
